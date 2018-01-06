7b162538a793dedf9963c9402689ae981b331e2b
Johnny Wong <hin.wong2@yahoo.com.au>
2018-01-07 01:59:47 +1100

    Remove unnecessary variable

--- Roulette simulator.R
+++ Roulette simulator.R
@@ -8,9 +8,7 @@ simulate_roulette <- function(sim_num = 10000,
   won <- 0
   for (simulation in 1:sim_num){
     current_money <- start_money
-    
-    count <- 0
-    
+
     while((current_money < start_money * goal_multiplier) && (current_money>0)){
       bid <- max(start_bid, start_money - current_money)
       if (runif(1) < (num_numbers - num_zeros)/2/num_numbers){
